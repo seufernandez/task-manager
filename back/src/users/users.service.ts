2272b5b23395f183e503ae3ae02757a5cb582f7a
Gabriel Fernandes <64269128+seufernandez@users.noreply.github.com>
2025-03-24 03:38:51 -0300

    chore(api): add user service input validation

--- back/src/users/users.service.ts
+++ back/src/users/users.service.ts
@@ -1,17 +1,24 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, BadRequestException, Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
-  
+
   async create(username: string, password: string): Promise<void> {
+    if (!username) {
+      throw new BadRequestException('Username is required');
+    }
+    if (!password) {
+      throw new BadRequestException('Password is required');
+    }
+
     const existingUser = await this.usersRepository.findOne(username);
     if (existingUser) {
       throw new ConflictException('Username already exists, set up a new one and try again');
     }
-  
+
     await this.usersRepository.create(username, password);
   }
 
